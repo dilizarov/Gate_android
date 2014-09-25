e3abed9d5bb4f57fd9acabafc18586480e942d08
David Ilizarov <dilizarov@gmail.com>
2014-09-25 04:05:21 -0700

    Created the base of the login/registration page. Have to add some dynamic stuff, but it looks like something :)

--- src/com/unlock/gate/LoginRegisterActivity.java
+++ src/com/unlock/gate/LoginRegisterActivity.java
@@ -15,7 +15,7 @@ public class LoginRegisterActivity extends Activity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.login_register, menu);
+		//getMenuInflater().inflate(R.menu.login_register, menu);
 		return true;
 	}
 
