e69990c23c05c5e800de2458450dfac088040099
David Ilizarov <dilizarov@gmail.com>
2014-12-16 01:35:47 -0800

    Adopted ViewHolder pattern for FeedListAdapter

--- Gate/app/src/main/java/com/unlock/gate/adapters/FeedListAdapter.java
+++ Gate/app/src/main/java/com/unlock/gate/adapters/FeedListAdapter.java
@@ -33,6 +33,20 @@ public class FeedListAdapter extends BaseAdapter {
     private Network network;
     private SharedPreferences mSessionPreferences;
 
+    static class ViewHolder {
+        TextView name;
+        TextView body;
+        TextView timestamp;
+        TextView commentsCount;
+        TextView networkName;
+        ImageView createComment;
+        ImageView commentsCountBubble;
+        ImageView smileyCount;
+        ImageView upPost;
+        TextView upCountPost;
+        LinearLayout postStats;
+    }
+
     public FeedListAdapter(Context context, List<Post> posts, Network network) {
         this.context = context;
         this.posts = posts;
@@ -56,8 +70,30 @@ public class FeedListAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder viewHolder;
+
         if (mInflater == null) mInflater = (LayoutInflater) context.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
-        if (convertView == null) convertView = mInflater.inflate(R.layout.feed_item, null);
+        if (convertView == null) {
+            convertView = mInflater.inflate(R.layout.feed_item, null);
+
+            viewHolder = new ViewHolder();
+
+            viewHolder.name                = (TextView) convertView.findViewById(R.id.name);
+            viewHolder.body                = (TextView) convertView.findViewById(R.id.body);
+            viewHolder.timestamp           = (TextView) convertView.findViewById(R.id.timestamp);
+            viewHolder.commentsCount       = (TextView) convertView.findViewById(R.id.commentsCount);
+            viewHolder.networkName         = (TextView) convertView.findViewById(R.id.networkName);
+            viewHolder.createComment       = (ImageView) convertView.findViewById(R.id.createComment);
+            viewHolder.commentsCountBubble = (ImageView) convertView.findViewById(R.id.commentsCountBubble);
+            viewHolder.smileyCount         = (ImageView) convertView.findViewById(R.id.smileyCount);
+            viewHolder.upPost              = (ImageView) convertView.findViewById(R.id.upPost);
+            viewHolder.upCountPost         = (TextView) convertView.findViewById(R.id.upCountPost);
+            viewHolder.postStats           = (LinearLayout) convertView.findViewById(R.id.postStats);
+
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
 
         if (position % 2 == 1) {
             convertView.setBackgroundColor(context.getResources().getColor(R.color.light_grey));
@@ -65,29 +101,16 @@ public class FeedListAdapter extends BaseAdapter {
             convertView.setBackgroundColor(context.getResources().getColor(R.color.white));
         }
 
-        TextView name          = (TextView) convertView.findViewById(R.id.name);
-        TextView body          = (TextView) convertView.findViewById(R.id.body);
-        TextView timestamp     = (TextView) convertView.findViewById(R.id.timestamp);
-        TextView commentsCount = (TextView) convertView.findViewById(R.id.commentsCount);
-        TextView networkName   = (TextView) convertView.findViewById(R.id.networkName);
-        ImageView createComment = (ImageView) convertView.findViewById(R.id.createComment);
-        ImageView commentsCountBubble = (ImageView) convertView.findViewById(R.id.commentsCountBubble);
-        final ImageView smileyCount  = (ImageView) convertView.findViewById(R.id.smileyCount);
-        final ImageView upPost       = (ImageView) convertView.findViewById(R.id.upPost);
-        final TextView upCountPost   = (TextView) convertView.findViewById(R.id.upCountPost);
-
-        final LinearLayout postStats = (LinearLayout) convertView.findViewById(R.id.postStats);
-
         final Post post = posts.get(position);
 
-        name.setText(post.getName());
-        body.setText(post.getBody());
-        timestamp.setText(post.getTimestamp());
+        viewHolder.name.setText(post.getName());
+        viewHolder.body.setText(post.getBody());
+        viewHolder.timestamp.setText(post.getTimestamp());
 
-        PostViewHelper.handleUpBehavior(context, post, upPost, upCountPost, smileyCount, postStats);
-        PostViewHelper.handleCommentBehavior(context, post, commentsCount, commentsCountBubble);
+        PostViewHelper.handleUpBehavior(context, post, viewHolder.upPost, viewHolder.upCountPost, viewHolder.smileyCount, viewHolder.postStats);
+        PostViewHelper.handleCommentBehavior(context, post, viewHolder.commentsCount, viewHolder.commentsCountBubble);
 
-        createComment.setOnClickListener(new View.OnClickListener() {
+        viewHolder.createComment.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
@@ -99,10 +122,10 @@ public class FeedListAdapter extends BaseAdapter {
         });
 
         if (network == null) {
-            networkName.setText(post.getNetworkName());
-            networkName.setVisibility(View.VISIBLE);
+            viewHolder.networkName.setText(post.getNetworkName());
+            viewHolder.networkName.setVisibility(View.VISIBLE);
         } else {
-            networkName.setVisibility(View.GONE);
+            viewHolder.networkName.setVisibility(View.GONE);
         }
 
         return convertView;
