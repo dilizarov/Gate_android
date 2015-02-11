49f0ea78460dee767b6b051e5557362789e5009b
David Ilizarov <dilizarov@gmail.com>
2015-02-11 14:15:16 -0600

    Should have the loading of gates in create post working now to mitigate a bug that was on some phones

--- Gate/app/src/main/java/com/unlock/gate/CreatePostActivity.java
+++ Gate/app/src/main/java/com/unlock/gate/CreatePostActivity.java
@@ -14,9 +14,15 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.unlock.gate.models.Gate;
+import com.unlock.gate.utils.APIRequestManager;
 import com.unlock.gate.utils.Butter;
 import com.unlock.gate.utils.CustomEditText;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -46,12 +52,6 @@ public class CreatePostActivity extends Activity {
             gates       = intent.getParcelableArrayListExtra("gates");
         }
 
-        final List<String> items = new ArrayList<String>();
-        for ( Gate gate : gates) items.add(gate.getName());
-
-        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_spinner_dropdown_item, items);
-
         gateSelection = (TextView) findViewById(R.id.gateSelection);
         writePost        = (ImageButton) findViewById(R.id.writePost);
         postBody         = (CustomEditText) findViewById(R.id.postBody);
@@ -60,36 +60,13 @@ public class CreatePostActivity extends Activity {
         if (!(postBody.getText().toString().trim().length() > 0))
             writePost.setEnabled(false);
 
-
-        gateSelection.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                new AlertDialog.Builder(CreatePostActivity.this)
-                               .setAdapter(adapter, new DialogInterface.OnClickListener() {
-                                   @Override
-                                   public void onClick(DialogInterface dialog, int which) {
-                                       currentGate = gates.get(which);
-                                       gateSelection.setText(items.get(which));
-
-                                       if (currentGate != null &&
-                                           postBody.getText().toString().trim().length() > 0) writePost.setEnabled(true);
-                                       else writePost.setEnabled(false);
-
-                                       dialog.dismiss();
-                                   }
-                               }).create().show();
-            }
-        });
-
         postBody.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
             }
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-
             }
 
             @Override
@@ -111,15 +88,121 @@ public class CreatePostActivity extends Activity {
             }
         });
 
-        if (currentGate != null) gateSelection.setText(currentGate.getName());
-
         if (intent.getStringExtra("postBody") != null) postBody.append(intent.getStringExtra("postBody"));
 
         if (intent.getStringExtra("errorMessage") != null) {
             Butter.between(this, intent.getStringExtra("errorMessage"));
         }
+
+        gateSelection.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (gates.isEmpty()) {
+                    gateSelection.setText("Loading Gates...");
+                    gateSelection.setEnabled(false);
+
+                    loadGates(true);
+                } else {
+                    final List<String> items = new ArrayList<String>();
+                    for (Gate gate : gates) items.add(gate.getName());
+
+                    final ArrayAdapter<String> adapter = new ArrayAdapter<String>(CreatePostActivity.this,
+                            android.R.layout.simple_spinner_dropdown_item, items);
+
+                    new AlertDialog.Builder(CreatePostActivity.this)
+                            .setTitle("Select a Gate")
+                            .setAdapter(adapter, new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    currentGate = gates.get(which);
+                                    gateSelection.setText(items.get(which));
+
+                                    if (currentGate != null &&
+                                            postBody.getText().toString().trim().length() > 0) writePost.setEnabled(true);
+                                    else writePost.setEnabled(false);
+
+                                    dialog.dismiss();
+                                }
+                            }).create().show();
+                }
+
+            }
+        });
+
+        if (gates.isEmpty()) {
+            gateSelection.setText("Loading Gates...");
+            gateSelection.setEnabled(false);
+
+            loadGates(false);
+        } else {
+            if (currentGate != null) gateSelection.setText(currentGate.getName());
+            else {
+                gateSelection.performClick();
+            }
+        }
     }
 
+    private void loadGates(final Boolean forceSelectGate) {
+        JSONObject params = new JSONObject();
+
+        Response.Listener<JSONObject> listener = new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(final JSONObject response) {
+
+                new Thread(new Runnable() {
+
+                    public void run() {
+                        gates.clear();
+
+                        JSONArray jsonGates = response.optJSONArray("gates");
+                        int len = jsonGates.length();
+                        for (int i = 0; i < len; i++) {
+                            JSONObject jsonGate = jsonGates.optJSONObject(i);
+                            Gate gate = new Gate(jsonGate.optString("external_id"),
+                                    jsonGate.optString("name"),
+                                    jsonGate.optInt("users_count"),
+                                    jsonGate.optJSONObject("creator").optString("name"));
+
+                            gates.add(gate);
+                        }
+
+                        runOnUiThread(new Runnable() {
+                            public void run() {
+                                if (gates.isEmpty()) {
+                                    Butter.between(CreatePostActivity.this, "No Gates Unlocked\nUnlock a Gate so you can post");
+
+                                    setResult(RESULT_CANCELED, getIntent());
+                                    finish();
+                                } else {
+                                    gateSelection.setText("Select a Gate");
+                                    gateSelection.setEnabled(true);
+
+                                    if (forceSelectGate) {
+                                        gateSelection.performClick();
+                                    }
+                                }
+                            }
+                        });
+                    }
+
+                }).start();
+            }
+        };
+
+        Response.ErrorListener errorListener = new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Butter.between(CreatePostActivity.this, "We couldn't load your Gates");
+
+                gateSelection.setText("Reload Gates");
+                gateSelection.setEnabled(true);
+            }
+        };
+
+        APIRequestManager.getInstance().doRequest().getGates(params, listener, errorListener);
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
